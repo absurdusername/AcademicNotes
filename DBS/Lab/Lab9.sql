a13b043387eebaea4716846302cdd33c030371cb
Ambuj Shukla <90390981+rottenketchup@users.noreply.github.com>
2024-02-29 21:20:43 +0530

    Indentation

--- DBS/Lab/Lab9.sql
+++ DBS/Lab/Lab9.sql
@@ -3,7 +3,7 @@
 
 create or replace procedure display_hello is
 begin
-	dbms_output.put_line('Good day to you.');
+    dbms_output.put_line('Good day to you.');
 end;
 /
 
@@ -12,25 +12,25 @@ end;
 
 create or replace procedure dept_info(v_dept_name department.dept_name%type) is
     cursor student_cursor is 
-    	select * from student where dept_name like v_dept_name;
+        select * from student where dept_name like v_dept_name;
 
-	cursor course_cursor is
+    cursor course_cursor is
         select * from course where dept_name like v_dept_name;
 begin
     dbms_output.put_line('== Department students ==');
-	for s in student_cursor loop
-		dbms_output.put_line(s.id || ' ' || s.name);
+    for s in student_cursor loop
+        dbms_output.put_line(s.id || ' ' || s.name);
     end loop;
 
-	dbms_output.put_line('== Department courses ==');
-	for c in course_cursor loop
-		dbms_output.put_line(c.course_id || ' ' || c.title);
+    dbms_output.put_line('== Department courses ==');
+    for c in course_cursor loop
+        dbms_output.put_line(c.course_id || ' ' || c.title);
     end loop;
 end;
 /
 
 begin
-	dept_info('&department');
+    dept_info('&department');
 end;
 /
 
@@ -38,24 +38,24 @@ end;
 -- ==========
 
 create or replace procedure course_popular(v_dept_name department.dept_name%type) is
-	cursor course_frequency is
-    	select course_id, title, count(*) as enrollment
+    cursor course_frequency is
+        select course_id, title, count(*) as enrollment
         from course
         where course.dept_name = v_dept_name
         group by course_id, title;
 
-	v_max_enrollment int := 0;
+    v_max_enrollment int := 0;
 begin
-	for c in course_frequency loop
-		if c.enrollment > v_max_enrollment then
-    		v_max_enrollment := c.enrollment;
-		end if;
+    for c in course_frequency loop
+        if c.enrollment > v_max_enrollment then
+            v_max_enrollment := c.enrollment;
+        end if;
     end loop;
 
-	dbms_output.put_line('Most popular courses for ' || v_dept_name);
+    dbms_output.put_line('Most popular courses for ' || v_dept_name);
     for c in course_frequency loop
-		if c.enrollment = v_max_enrollment then
-			dbms_output.put_line(c.course_id || ' ' || c.title);
+        if c.enrollment = v_max_enrollment then
+            dbms_output.put_line(c.course_id || ' ' || c.title);
         end if;
     end loop;
 end;
@@ -63,7 +63,7 @@ end;
 
 begin
     for r in (select * from department) loop
-		course_popular(r.dept_name);
+        course_popular(r.dept_name);
     end loop;
 end;
 /
@@ -94,69 +94,69 @@ create or replace function department_highest(v_dept_name department.dept_name%t
     return instructor.name%type is
 
     cursor instructor_cursor is
-    	select * from instructor where dept_name like v_dept_name;
+        select * from instructor where dept_name like v_dept_name;
 
-	v_highest_salary instructor.salary%type := 0;
-	v_name instructor.name%type;
+    v_highest_salary instructor.salary%type := 0;
+    v_name instructor.name%type;
 begin
-	for i in instructor_cursor loop
-		if i.salary > v_highest_salary then
-			v_name := i.name;
-			v_highest_salary := i.salary;
-    	end if;
+    for i in instructor_cursor loop
+        if i.salary > v_highest_salary then
+            v_name := i.name;
+            v_highest_salary := i.salary;
+        end if;
     end loop;
 
-	return v_name;
+    return v_name;
 end;
 /
 
 declare
-	cursor dept_cursor is 
-		select distinct dept_name from department;
+    cursor dept_cursor is 
+        select distinct dept_name from department;
 begin
-	for r in dept_cursor loop
-		dbms_output.put_line(r.dept_name || ': ' || department_highest(r.dept_name));
-	end loop;
+    for r in dept_cursor loop
+        dbms_output.put_line(r.dept_name || ': ' || department_highest(r.dept_name));
+    end loop;
 end;
 /
 
 -- Question 7
 -- ==========
 create or replace package department_operations is
-	procedure list_instructors(v_dept_name department.dept_name%type);
-	function max_salary(v_dept_name department.dept_name%type) return number;
+    procedure list_instructors(v_dept_name department.dept_name%type);
+    function max_salary(v_dept_name department.dept_name%type) return number;
 end department_operations;
 /
 
 create or replace package body department_operations is 
-	procedure list_instructors(v_dept_name department.dept_name%type) is
-    	cursor instructor_cursor is
-    		select name from instructor where dept_name like v_dept_name;
-	begin
+    procedure list_instructors(v_dept_name department.dept_name%type) is
+        cursor instructor_cursor is
+            select name from instructor where dept_name like v_dept_name;
+    begin
         dbms_output.put_line('== ' || v_dept_name || ' instructors ==');
-		for i in instructor_cursor loop
-			dbms_output.put_line(i.name);
+        for i in instructor_cursor loop
+            dbms_output.put_line(i.name);
         end loop;
     end;
 
-	function max_salary(v_dept_name department.dept_name%type) return number is
+    function max_salary(v_dept_name department.dept_name%type) return number is
         cursor salary_cursor is
-        	select salary from instructor where dept_name like v_dept_name;
-		v_max_salary number := 0;
+            select salary from instructor where dept_name like v_dept_name;
+        v_max_salary number := 0;
     begin
         for i in salary_cursor loop
-			if i.salary > v_max_salary then
-        		v_max_salary := i.salary;
-			end if;
+            if i.salary > v_max_salary then
+                v_max_salary := i.salary;
+            end if;
         end loop;
-		return v_max_salary;
+        return v_max_salary;
     end;
 end department_operations;
 /
 
 begin
-	department_operations.list_instructors('Comp. Sci.');
-	dbms_output.put_line('Highest salary: ' ||
+    department_operations.list_instructors('Comp. Sci.');
+    dbms_output.put_line('Highest salary: ' ||
         department_operations.max_salary('Comp. Sci.'));
 end;
 /
@@ -170,31 +170,31 @@ create or replace procedure compute_interest(
 ) is
 begin
     total_sum := principal;
-	for i in 1..years loop 
-		total_sum := total_sum * (1 + roi);
-	end loop;
-	
-	compound_interest := total_sum - principal;
-	simple_interest := roi * years * principal;
+    for i in 1..years loop 
+        total_sum := total_sum * (1 + roi);
+    end loop;
+    
+    compound_interest := total_sum - principal;
+    simple_interest := roi * years * principal;
 end;
 /
 
 declare
     -- inputs
     principal number := 100;
-	roi number := 0.09;
-	years pls_integer := 8;
+    roi number := 0.09;
+    years pls_integer := 8;
 
-	-- outputs
+    -- outputs
     total_sum number;
-	simple_interest number;
-	compound_interest number;
+    simple_interest number;
+    compound_interest number;
 begin
-	compute_interest(principal, roi, years, 
-    	total_sum, simple_interest, compound_interest);
+    compute_interest(principal, roi, years, 
+        total_sum, simple_interest, compound_interest);
 
-	dbms_output.put_line('Total sum: ' || total_sum);
-	dbms_output.put_line('Simple interest: ' || simple_interest);
-	dbms_output.put_line('Compound interest: ' || compound_interest);
+    dbms_output.put_line('Total sum: ' || total_sum);
+    dbms_output.put_line('Simple interest: ' || simple_interest);
+    dbms_output.put_line('Compound interest: ' || compound_interest);
 end;
 /
