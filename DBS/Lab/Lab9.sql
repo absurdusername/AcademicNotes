d93de5d47312fe19bb3c6296c1025ce437c993d8
Ambuj Shukla <90390981+rottenketchup@users.noreply.github.com>
2024-02-29 21:07:00 +0530

    Complete lab 9

--- DBS/Lab/Lab9.sql
+++ DBS/Lab/Lab9.sql
@@ -112,3 +112,81 @@ begin
 		dbms_output.put_line(r.dept_name || ': ' || department_highest(r.dept_name));
 	end loop;
 end;
+
+-- Question 7
+-- ==========
+
+create or replace package department_operations is
+	procedure list_instructors(v_dept_name department.dept_name%type);
+	function max_salary(v_dept_name department.dept_name%type) return number;
+end department_operations;
+
+create or replace package body department_operations is 
+	procedure list_instructors(v_dept_name department.dept_name%type) is
+    	cursor instructor_cursor is
+    		select name from instructor where dept_name like v_dept_name;
+	begin
+        dbms_output.put_line('== ' || v_dept_name || ' instructors ==');
+		for i in instructor_cursor loop
+			dbms_output.put_line(i.name);
+        end loop;
+    end;
+
+	function max_salary(v_dept_name department.dept_name%type) return number is
+        cursor salary_cursor is
+        	select salary from instructor where dept_name like v_dept_name;
+		v_max_salary number := 0;
+    begin
+        for i in salary_cursor loop
+			if i.salary > v_max_salary then
+        		v_max_salary := i.salary;
+			end if;
+        end loop;
+		return v_max_salary;
+    end;
+end department_operations;
+
+begin
+	department_operations.list_instructors('Comp. Sci.');
+	dbms_output.put_line('Highest salary: ' ||
+        department_operations.max_salary('Comp. Sci.'));
+end;
+
+-- Question 8
+-- ==========
+
+create or replace procedure compute_interest(
+    principal number, roi number, years pls_integer,
+    total_sum IN OUT number, 
+    simple_interest OUT number, compound_interest OUT number
+) is
+begin
+    total_sum := principal;
+	for i in 1..years loop 
+		total_sum := total_sum * (1 + roi);
+	end loop;
+	
+	compound_interest := total_sum - principal;
+	simple_interest := roi * years * principal;
+end;
+/
+
+declare
+    -- inputs
+    principal number := 100;
+	roi number := 0.09;
+	years pls_integer := 8;
+
+	-- outputs
+    total_sum number;
+	simple_interest number;
+	compound_interest number;
+begin
+	compute_interest(principal, roi, years, 
+    	total_sum, simple_interest, compound_interest);
+
+	dbms_output.put_line('Total sum: ' || total_sum);
+	dbms_output.put_line('Simple interest: ' || simple_interest);
+	dbms_output.put_line('Compound interest: ' || compound_interest);
+end;
+/
--- DBS/Notes/9_Packages.md
+++ DBS/Notes/9_Packages.md
@@ -3,17 +3,18 @@
 ### Package specification
 
 ```sql
-create package asdfghjkl as
+create package asdfghjkl is
     procedure foo;
     procedure bar(arg1 int);
-    -- died_of_laughter exception;
+    died_of_laughter exception;
 end asdfghjkl;
 ```
 
 ### Package body
 ```sql
-create or replace package body asdfghjkl as
+create or replace package body asdfghjkl is
     procedure foo is
+    begin
         dbms_output.put_line('foo');
     end;
 
@@ -28,5 +29,15 @@ end asdfghjkl;
 ```sql
 exec asdfghjkl.foo;
 exec asdfghjkl.bar(1);
+
+begin
+    asdfghjkl.foo;
+end;
 ```
 
+### IN, OUT, and IN OUT
+All procedure and function parameters are marked as `IN` by default. These parameters cannot be modified. Treat them like constants.
+
+`OUT` parameters cannot be read or accessed (not sure), they can only be assigned values to. Assigning values to them modifies the original variable used in the function call. Think of them like pointers.
+
+`IN OUT` is almost like `OUT` but the parameter can be read.
